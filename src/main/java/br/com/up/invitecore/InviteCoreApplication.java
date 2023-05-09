06591096a7d6bdfabde49142d4c529593abeb79c
Ailton Piedade de Moura <apdmoura@rd.com.br>
2023-05-08 23:24:24 -0300

    criacao projeto

---
+++ src/main/java/br/com/up/invitecore/InviteCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.up.invitecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InviteCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/up/invitecore/InviteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.up.invitecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
