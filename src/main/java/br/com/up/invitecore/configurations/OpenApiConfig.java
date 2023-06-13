f626a5d59780caaf6bbb7d3ee77058a2b8ccae9f
apdmoura <apdmoura@gmail.com>
2023-06-13 17:38:32 -0300

    add swagger

---
+++ src/main/java/br/com/up/invitecore/configurations/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package br.com.up.invitecore.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Invite API")
+                        .description("Invite API for App")
+                        .version("v1"));
+    }
+
+}
--- src/main/java/br/com/up/invitecore/controllers/UserController.java
+++ src/main/java/br/com/up/invitecore/controllers/UserController.java
@@ -1,5 +1,12 @@
 package br.com.up.invitecore.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +23,60 @@ import br.com.up.invitecore.services.UserService;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "Endpoint for managing User")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
-	
+
+	@Operation(summary = "Create User", description = "Create User",
+		tags = "User",
+		responses = {
+			@ApiResponse(description = "Sucess", responseCode = "200",
+				content = @Content(schema = @Schema(implementation = UserDTO.class))
+			),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+		}
+	)
 	@PostMapping("/create")
 	public ResponseEntity<UserDTO> create(@RequestBody UserDTO user) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(userService.save(user).toDTO());
 	}
-	
+
+	@Operation(summary = "Find User", description = "Find User",
+			tags = "User",
+			responses = {
+					@ApiResponse(description = "Sucess", responseCode = "200",
+							content = @Content(schema = @Schema(implementation = UserDTO.class))
+					),
+					@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+			}
+	)
 	@GetMapping("/find/{id}")
 	public ResponseEntity<UserDTO> find(@PathVariable Long id) {
 		return ResponseEntity.ok(userService.find(id).toDTO());
 	}
-	
+
+	@Operation(summary = "Update User", description = "Update User",
+			tags = "User",
+			responses = {
+					@ApiResponse(description = "Sucess", responseCode = "200",
+							content = @Content(schema = @Schema(implementation = UserDTO.class))
+					),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+			}
+	)
 	@PutMapping("/update/{id}")
 	public ResponseEntity<UserDTO> update(@RequestBody UserDTO user, @PathVariable Long id) {
 		return ResponseEntity.ok(userService.update(user, id).toDTO());
